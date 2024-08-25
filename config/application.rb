eb954e3b9b63b76a37d1801d76793f8d7126e2cc
wkwon <rnjsdntjr26@daum.net>
2024-08-25 18:30:04 +0900

    feat: github action 추가

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module NaverCafePostCreator
 
     # Configuration for the application, engines, and railties goes here.
     #
-    # These settings can be overridden in specific environments using the files
+    # These settings can be overridden in specific environments using the file
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
